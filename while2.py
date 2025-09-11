f27c1b1001c9cfe60eac8b0ca5af81ca3b1aaa86
tucoema <murilinhodejesusbasiliotkd@gmail.com>
2025-09-11 10:02:30 -0300

    Add files via upload

---
+++ while2.py
@@ -0,0 +1,4 @@
+numero = 101
+while numero <= 199:
+	print(numero)
+	numero += 2
---
+++ whilee3.py
@@ -0,0 +1,8 @@
+N = int(input("Digite um número inteiro N: "))
+soma = 0
+contador = 1
+while contador <= N:
+	soma += contador
+	contador += 1
+print(f"A soma dos números de 1 até", N , soma)
+n = int(input("digite um numero"))
