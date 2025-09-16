0b4becc1729505cb17b03bfddf1b409e6a09d374
tucoema <murilinhodejesusbasiliotkd@gmail.com>
2025-09-16 08:45:13 -0300

    Add files via upload

---
+++ operaçoes matematicas.py
@@ -0,0 +1,24 @@
+print("************MENU DE OPERAÇÕES MATEMATICAS************")
+print("-----------------------------------------------------")
+print("escolhas a sua operaçao matematica")
+print("1-soma")
+print("2-subtração")
+print("3-multiplicação")
+print("4-divisão")
+print("5-par ou impar")
+print("6-fatorial")
+print("7-tabuada")
+print("8-sair")
+numero=int(input("digite um numero:"))
+operação=int(input("digite uma operação:"))
+if operação==1:
+    numero2=int(input("digite outro numero:"))
+    resultado=numero+numero2
+    print("o resultado é:", resultado)
+if operação==2:
+    numero2=int(input("digite um numero"))
+    resultado=numero-numero2
+    
+     
+
+    
